bbdddeaeaff71519f12025b9769e861bf213e68c
arpruss@gmail.com <arpruss@gmail.com@1778d010-7dfa-e1c8-3906-7cc4988ff794>
2012-07-08 18:48:14 +0000

    icon

---
+++ scale.sh
@@ -0,0 +1,4 @@
+convert rocket512.png -resize 114x114 rocket114.png
+convert rocket512.png -resize 72x72 res/drawable-hdpi/icon.png
+convert rocket512.png -resize 48x48 res/drawable-mdpi/icon.png
+convert rocket512.png -resize 32x32 res/drawable-ldpi/icon.png
